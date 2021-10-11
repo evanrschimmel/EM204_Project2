b568db65f9cb417df205a6286f3fd25925dd0474
Evan Schimmel <evanrschimmel@gmail.com>
2021-10-11 16:08:43 -0400

    Create basic script for data pull

---
+++ Schimmel_Evan_EM204_project_2.m
@@ -0,0 +1,17 @@
+%****************************************************************
+%   Schimmel_Evan_EM204_project_2.m
+%
+%   PROGRAM DESCRIPTION
+%   Add before submitting
+%
+%   WRITTEN BY: Evan Schimmel
+%               10/11/2021
+%
+%****************************************************************
+
+clear all
+close all
+clc
+
+round_tube_data = readmatrix('RoundTube_Options');
+round_bar_data = readmatrix('RoundBar_Options');
